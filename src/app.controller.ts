eeb5c38e5ac81d05ba4527e2b5131f82b6995efe
jeny3g <mirandajean2009@gmail.com>
2022-12-12 22:26:14 -0300

    feat: :sparkles: adding validation for notifications

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { AppService } from './app.service';
+import { CreateNotificationBody } from './create-notification-body';
 import { PrismaService } from './prisma.service';
 
 @Controller('notifications')
@@ -16,12 +17,15 @@ export class AppController {
   }
 
   @Post()
-  create() {
+  create(@Body() body: CreateNotificationBody) {
+    console.log(body);
+    const { recipientId, content, category } = body;
+
     return this.prisma.notification.create({
       data: {
-        category: 'category',
-        content: 'content',
-        recipientId: 'Zachery',
+        content,
+        category,
+        recipientId,
         id: randomUUID(),
       },
     });
