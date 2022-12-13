e16a79215d093ef18d5ee250f8d293fdc7806f3f
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2022-12-12 22:07:22 -0300

    feat: :sparkles: installing prisma & creating notifications db

---
+++ README.md
@@ -0,0 +1,24 @@
+## Install prisma
+```bash
+npm i prisma -D
+```
+
+## Install prisma production
+```bash
+npm i @prisma/client
+```
+
+## Prisma init
+```bash
+npx prisma init --datasource-provider SQLite
+```
+
+## Prisma create database
+```bash
+npx prisma migrate dev
+```
+
+## Prisma show database
+```bash
+npx prisma studio
+```
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
-@Controller('app')
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
-  @Get('hello')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get()
+  getHello() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  create() {
+    return this.prisma.notification.create({
+      data: {
+        category: 'category',
+        content: 'content',
+        recipientId: 'Zachery',
+        id: randomUUID(),
+      },
+    });
   }
 }
