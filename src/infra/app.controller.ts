26c9e40781cc2a5e6b7a2c1a045e805e7fce0706
jeny3g <mirandajean2009@gmail.com>
2022-12-14 12:09:31 -0300

    refactor: :hammer: moving files to infra

--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -1,8 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { AppService } from './app.service';
-import { CreateNotificationBody } from './create-notification-body';
-import { PrismaService } from './prisma.service';
+import { CreateNotificationBody } from './infra/create-notification-body';
+import { PrismaService } from './infra/prisma.service';
 
 @Controller('notifications')
 export class AppController {
