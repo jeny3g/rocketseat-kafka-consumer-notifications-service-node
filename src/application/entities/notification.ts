940453fa44ef5940ba6f29f325212a362494bcb1
jeny3g <mirandajean2009@gmail.com>
2022-12-14 19:28:28 -0300

    refactor: :hammer: connecting code with infra

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto';
 import { Replace } from 'src/helpers/Replace';
 import { Content } from './content';
 
@@ -10,48 +11,54 @@ export interface NotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: NotificationProps;
 
   constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt || new Date(),
     };
   }
 
-  get recipientId(): string {
+  public get id(): string {
+    return this._id;
+  }
+
+  public get recipientId(): string {
     return this.props.recipientId;
   }
 
-  set recipientId(recipientId: string) {
+  public set recipientId(recipientId: string) {
     this.props.recipientId = recipientId;
   }
 
-  get content(): Content {
+  public get content(): Content {
     return this.props.content;
   }
 
-  set content(content: Content) {
+  public set content(content: Content) {
     this.props.content = content;
   }
 
-  get category(): string {
+  public get category(): string {
     return this.props.category;
   }
 
-  set category(category: string) {
+  public set category(category: string) {
     this.props.category = category;
   }
 
-  get readAt(): Date | null | undefined {
+  public get readAt(): Date | null | undefined {
     return this.props.readAt;
   }
 
-  set readAt(readAt: Date | null | undefined) {
+  public set readAt(readAt: Date | null | undefined) {
     this.props.readAt = readAt;
   }
 
-  get createdAt(): Date {
+  public get createdAt(): Date {
     return this.props.createdAt;
   }
 }
