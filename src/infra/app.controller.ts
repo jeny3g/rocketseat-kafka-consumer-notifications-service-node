2796d3412cfa90d12fe04c0414a16e5f42d8d8b8
jeny3g <mirandajean2009@gmail.com>
2022-12-14 12:32:59 -0300

    refactor: :hammer: configuring jest

--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -1,15 +1,11 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { randomUUID } from 'crypto';
-import { AppService } from './app.service';
-import { CreateNotificationBody } from './infra/create-notification-body';
-import { PrismaService } from './infra/prisma.service';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prisma.service';
 
 @Controller('notifications')
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly prisma: PrismaService,
-  ) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
   getHello() {
