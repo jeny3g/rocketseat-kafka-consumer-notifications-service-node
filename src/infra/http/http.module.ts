5e0d8cfce41cfbed23550ca2c5d6658bdea2eac9
jeny3g <mirandajean2009@gmail.com>
2022-12-16 18:53:43 -0300

    feat: :sparkles: creating controllers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,4 +1,9 @@
-import { SendNotification } from '@application/use-cases/send-notification';
+import { CancelNotification } from '@application/use-cases/cancel-notification/cancel-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification.ts/read-notification';
+import { SendNotification } from '@application/use-cases/send-notification/send-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification/unread-notification';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
@@ -6,6 +11,13 @@ import { NotificationsController } from './controllers/notifications.controller'
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
